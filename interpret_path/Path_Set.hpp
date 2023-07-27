7c998b4cae2c04a60099113567f4c3aad31a2413
molhot <86003237+molhot@users.noreply.github.com>
2023-07-27 12:55:32 +0000

    tmp push 最長一致を探すところで難儀中

--- interpret_path/Path_Set.hpp
+++ interpret_path/Path_Set.hpp
@@ -5,27 +5,48 @@
 class path_set
 {
     private:
-        std::map<std::string, int>          prifex_pathset;
+        std::vector<std::string>    perfectpath_set;
+        std::vector<std::string>    forwardconstent_set;
+        std::vector<std::string>    noprefex_set;
 
     public:
         path_set();
         ~path_set();
-        std::map<std::string, int>    get_pathset();
+        std::vector<std::string>    get_perfectpath();
+        std::vector<std::string>    get_forwardconstent();
+        std::vector<std::string>    get_noprefex();
 };
 
 //1が= 2が^~ 3がなし
 
 path_set::path_set()
 {
-    this->prifex_pathset["/sample/go"] = 3;
-    this->prifex_pathset["/sample/bianchi/towl"] = 3;
-    this->prifex_pathset["/sample/go/ambulance"] = 3;
-    this->prifex_pathset["/"] = 3;
+    this->perfectpath_set.push_back("/sample/go");
+    this->perfectpath_set.push_back("/sampleple/index.html");
+    this->forwardconstent_set.push_back("/sample/cinema/mayo");
+    this->noprefex_set.push_back("/aba/ee");
+    this->noprefex_set.push_back("/sample/go");
+    this->noprefex_set.push_back("/sample/cinema/mayo/fanta");
+    this->noprefex_set.push_back("/sample/go");
+    this->noprefex_set.push_back("/sampleple/index.html");
+    this->noprefex_set.push_back("/aaa/cc");
+    this->noprefex_set.push_back("/aaa/dd");
+    this->noprefex_set.push_back("/aaa");
 }
 
-std::map<std::string, int>    path_set::get_pathset()
+std::vector<std::string>    path_set::get_perfectpath()
 {
-    return (this->prifex_pathset);
+    return (this->perfectpath_set);
+}
+
+std::vector<std::string>    path_set::get_forwardconstent()
+{
+    return (this->forwardconstent_set);
+}
+
+std::vector<std::string>    path_set::get_noprefex()
+{
+    return (this->noprefex_set);
 }
 
 path_set::~path_set()
--- interpret_path/main.cpp
+++ interpret_path/main.cpp
@@ -10,13 +10,13 @@
 //そのpathにあわせてpathsetから適切なpathを返すとする
 //この挙動をnginxに準拠させる
 
-std::string check_fullmatch(std::map<std::string, int> set, std::string path)
+std::string check_fullmatch(std::vector<std::string> set, std::string path)
 {
-    std::map<std::string, int>::iterator it = set.begin();
+    std::vector<std::string>::iterator it = set.begin();
     while (it != set.end())
     {
-        if (it->first == path)
-            return ("FULL MATCH ! | " + it->first);
+        if (*it == path)
+            return ("FULL MATCH ! | " + *it);
         it++;
     }
     return ("");
@@ -35,39 +35,50 @@ size_t  compare_twowords(std::string locationpath, std::string path)
     return (pos);
 }
 
-std::string check_longestmatchpath(std::map<std::string, int> set, std::string path)
+std::string check_longestmatchpath(\
+std::vector<std::string> forwardconstent_set, \
+std::vector<std::string> noprefex_set, \
+std::string path)
 {
-    std::string ans = "";
-    size_t  longestmatch_length = 0;
-    size_t  tmp_len = 0;
-    std::map<std::string, int>::iterator it = set.begin();
-    while (it != set.end())
+    std::string                         ans = "";
+    size_t                              longestmatch_length = 0;
+    size_t                              tmp_len = 0;
+    std::vector<std::string>::iterator  it = forwardconstent_set.begin();
+
+    while (it != forwardconstent_set.end())
     {
-        if ((it->first).length() < path.length())
+        if ((*it).length() < path.length())
         {
-            tmp_len = compare_twowords((it->first), path);
-            if ((it->first).length() == tmp_len && tmp_len >= longestmatch_length)
+            tmp_len = compare_twowords((*it), path);
+            if ((*it).length() == tmp_len && tmp_len >= longestmatch_length)
             {
                 longestmatch_length = tmp_len;
-                ans = (it->first);
+                ans = (*it);
             }
         }
         it++;
     }
+
+    std::vector<std::string>::iterator  it = noprefex_set.begin();
+
     if (ans != "")
         return ("TEMP MATCH ! | " + ans);
     else
         return ("");
 }
 
-std::string interpret_path(std::map<std::string, int> set, std::string substring)
+std::string interpret_path(std::vector<std::string> perfectmatch_set, \
+std::vector<std::string> forwardconstent_set, \
+std::vector<std::string> noprefex_set, std::string substring)
 {
     std::string anser;
-    //完全一致するものがあるか確認
-    anser = check_fullmatch(set, substring);
+    // 完全一致するものがあるか確認
+    anser = check_fullmatch(perfectmatch_set, substring);
+    // 完全一致の場合は内部リダイレクトがあればそれを評価する
+    // return (eval_perfectmatch())
     if (anser != "")
         return (anser);
-    anser = check_longestmatchpath(set, substring);
+    anser = check_longestmatchpath(forwardconstent_set, noprefex_set, substring);
     if (anser != "")
         return (anser);
     return ("NOT MATCH");
@@ -75,7 +86,7 @@ std::string interpret_path(std::map<std::string, int> set, std::string substring
 
 int main()
 {
-    std::string subjectpath = "/mogumogu";
+    std::string subjectpath = "/sample/go/further";
     path_set    set;
-    std::cout << interpret_path(set.get_pathset(), subjectpath) << std::endl;
+    std::cout << interpret_path(set.get_perfectpath(), set.get_forwardconstent(), set.get_noprefex(), subjectpath) << std::endl;
 }
